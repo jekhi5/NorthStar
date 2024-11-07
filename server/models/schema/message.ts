e1fd5f430499e3dbdcce2072a4f74fc08645b414
Ashley Davis <ashley921davis@gmail.com>
2024-11-07 17:01:44 -0500

    another small typo

--- server/models/schema/message.ts
+++ server/models/schema/message.ts
@@ -6,7 +6,7 @@ import { Schema } from 'mongoose';
  * Each message includes the following fields:
  * - `content`: The content of the message.
  * - `sentBy`: The the user who wrote the message.
- * - `sentDateTime`: The date and time when the message was created.
+ * - `sentDateTime`: The date and time when the message was sent.
  */
 const messageSchema: Schema = new Schema(
   {
