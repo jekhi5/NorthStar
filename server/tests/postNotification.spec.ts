899cf8919b6083fa5b3430eabf20c7087e54a36f
Jacob Kline <jacobk513@gmail.com>
2024-11-12 09:58:20 -0500

    remove lint in test file

--- server/tests/postNotification.spec.ts
+++ server/tests/postNotification.spec.ts
@@ -15,38 +15,6 @@ const answer1User: User = {
   postNotifications: [],
 };
 
-const answer2User: User = {
-  uid: 'ab531bcf520c19729de860ea',
-  username: 'Answer2User',
-  email: 'answer2User@email.com',
-  status: 'Not endorsed',
-  postNotifications: [],
-};
-
-const answer3User: User = {
-  uid: 'ab53191e810c197aaae860ea',
-  username: 'Answer3User',
-  email: 'answer3User@email.com',
-  status: 'Not endorsed',
-  postNotifications: [],
-};
-
-const answer4User: User = {
-  uid: 'ab51234e810c19729de860ea',
-  username: 'Answer4User',
-  email: 'answer4User@email.com',
-  status: 'Not endorsed',
-  postNotifications: [],
-};
-
-const question1User: User = {
-  uid: 'ab51234e810c19729deef0ea',
-  username: 'Question1User',
-  email: 'question1User@email.com',
-  status: 'Not endorsed',
-  postNotifications: [],
-};
-
 const question2User: User = {
   uid: 'ab5123abc10c19729deef0ea',
   username: 'Question2User',
@@ -63,17 +31,6 @@ const question3User: User = {
   postNotifications: [],
 };
 
-const tag1: Tag = {
-  _id: new mongoose.Types.ObjectId('507f191e810c19729de860ea'),
-  name: 'tag1',
-  description: 'tag1 description',
-};
-const tag2: Tag = {
-  _id: new mongoose.Types.ObjectId('65e9a5c2b26199dbcc3e6dc8'),
-  name: 'tag2',
-  description: 'tag2 description',
-};
-
 const ans1: Answer = {
   _id: new mongoose.Types.ObjectId('65e9b58910afe6e94fc6e6dc'),
   text: 'Answer 1 Text',
@@ -83,81 +40,6 @@ const ans1: Answer = {
   downVotes: [],
   comments: [],
 };
-
-const ans2: Answer = {
-  _id: new mongoose.Types.ObjectId('65e9b58910afe6e94fc6e6dd'),
-  text: 'Answer 2 Text',
-  ansBy: answer2User,
-  ansDateTime: new Date('2024-06-10'),
-  upVotes: [],
-  downVotes: [],
-  comments: [],
-};
-
-const ans3: Answer = {
-  _id: new mongoose.Types.ObjectId('65e9b58910afe6e94fc6e6df'),
-  text: 'Answer 3 Text',
-  ansBy: answer3User,
-  ansDateTime: new Date('2024-06-11'),
-  upVotes: [],
-  downVotes: [],
-  comments: [],
-};
-
-const ans4: Answer = {
-  _id: new mongoose.Types.ObjectId('65e9b58910afe6e94fc6e6de'),
-  text: 'Answer 4 Text',
-  ansBy: answer4User,
-  ansDateTime: new Date('2024-06-14'),
-  upVotes: [],
-  downVotes: [],
-  comments: [],
-};
-
-const MOCK_QUESTIONS: Question[] = [
-  {
-    _id: new mongoose.Types.ObjectId('65e9b58910afe6e94fc6e6dc'),
-    title: 'Question 1 Title',
-    text: 'Question 1 Text',
-    tags: [tag1],
-    answers: [ans1],
-    askedBy: question1User,
-    askDateTime: new Date('2024-06-03'),
-    views: ['question1_user', 'question2_user'],
-    upVotes: [],
-    downVotes: [],
-    comments: [],
-    subscribers: [],
-  },
-  {
-    _id: new mongoose.Types.ObjectId('65e9b5a995b6c7045a30d823'),
-    title: 'Question 2 Title',
-    text: 'Question 2 Text',
-    tags: [tag2],
-    answers: [ans2, ans3],
-    askedBy: question2User,
-    askDateTime: new Date('2024-06-04'),
-    views: ['question1_user', 'question2_user', 'question3_user'],
-    upVotes: [],
-    downVotes: [],
-    comments: [],
-    subscribers: [],
-  },
-  {
-    _id: new mongoose.Types.ObjectId('34e9b58910afe6e94fc6e99f'),
-    title: 'Question 3 Title',
-    text: 'Question 3 Text',
-    tags: [tag1, tag2],
-    answers: [ans4],
-    askedBy: question3User,
-    askDateTime: new Date('2024-06-03'),
-    views: ['question3_user'],
-    upVotes: [],
-    downVotes: [],
-    comments: [],
-    subscribers: [],
-  },
-];
 
 const com1: Comment = {
   _id: new mongoose.Types.ObjectId('65e9b716ff0e89abc6b2de01'),
