e1196618e3d7d8fc7a629a6863e935de9603159a
Jacob Kline <jacobk513@gmail.com>
2024-11-14 09:56:46 -0500

    readding isUserValid

--- client/src/hooks/useSignUp.ts
+++ client/src/hooks/useSignUp.ts
@@ -3,7 +3,7 @@ import { ChangeEvent, useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
 import useLoginContext from './useLoginContext';
-import { addUser, checkUsernameAvailability, getUserByUid } from '../services/userService';
+import { addUser, checkValidUser, getUserByUid } from '../services/userService';
 import { User } from '../types';
 
 /**
@@ -85,10 +85,10 @@ const useSignUp = () => {
     if (!validateForm()) return;
 
     try {
-      // Check if username is available
-      const isUsernameAvailable = await checkUsernameAvailability(formData.username);
-      if (!isUsernameAvailable) {
-        setError('Username is already taken, please choose a different one');
+      // Check if username and email is available
+      const isValidUser = await checkValidUser(formData.username, formData.email);
+      if (!isValidUser.available) {
+        setError(isValidUser.message);
         return;
       }
 
