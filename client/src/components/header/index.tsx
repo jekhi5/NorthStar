cf7c8af061c7f5ec65d4b054e7ff994cd3e61c5c
getheobald <gracelyntheobald@gmail.com>
2024-11-18 17:03:17 -0500

    styling

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import { NavLink } from 'react-router-dom';
 import useHeader from '../../hooks/useHeader';
 import './index.css';
 
 /**
- * Header component that renders the main title and a search bar.
+ * Header component that renders the main title, navigation links, and a search bar.
  * The search bar allows the user to input a query and navigate to the search results page
  * when they press Enter.
  */
@@ -14,6 +14,26 @@ const Header = () => {
     <div id='header' className='header'>
       <div></div>
       <div className='title'>Fake Stack Overflow</div>
+      <div className='headerNav'>
+        <NavLink
+          to='/home'
+          id='menu_questions'
+          className={({ isActive }) => `menu_button ${isActive ? 'menu_selected' : ''}`}>
+          Questions
+        </NavLink>
+        <NavLink
+          to='/tags'
+          id='menu_tag'
+          className={({ isActive }) => `menu_button ${isActive ? 'menu_selected' : ''}`}>
+          Tags
+        </NavLink>
+        <NavLink
+          to='/chatroom'
+          id='menu_chatroom'
+          className={({ isActive }) => `menu_button ${isActive ? 'menu_selected' : ''}`}>
+          Chatroom
+        </NavLink>
+      </div>
       <input
         id='searchBar'
         placeholder='Search ...'
@@ -22,6 +42,18 @@ const Header = () => {
         onChange={handleInputChange}
         onKeyDown={handleKeyDown}
       />
+      <div className='notifications-and-profile'>
+        <div className='notifications-icon'>
+          <NavLink to='/notifications'>
+            <span className='material-symbols-outlined'>circle_notifications</span>
+          </NavLink>
+        </div>
+        <div className='profile-icon'>
+          <NavLink to='/profile'>
+            <span className='material-symbols-outlined'>account_circle</span>
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 };
