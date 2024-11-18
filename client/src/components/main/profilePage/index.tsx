99f33564dcfc366888520712451e4b9e35843450
Ashley Davis <ashley921davis@gmail.com>
2024-11-17 22:04:35 -0500

    lint

--- client/src/components/main/profilePage/index.tsx
+++ client/src/components/main/profilePage/index.tsx
@@ -81,23 +81,24 @@ const ProfilePage = () => {
             <p className='profile-username'>Username: {profile.username}</p>
             <p className='profile-email'>Email: {profile.email}</p>
             <div className='profile-status'>
-            <ul>
-              <li>
-                Progress towards Endorsed: {calculateReputationPercentage(profile.reputation, 30)}%
-              </li>
-              <li>
-                Progress towards Super Smarty Pants:{' '}
-                {calculateReputationPercentage(profile.reputation, 100)}%
-              </li>
-              <li>
-                Progress towards Mentor: {calculateReputationPercentage(profile.reputation, 500)}%
-              </li>
-              <li>
-                Progress towards Grandmaster:{' '}
-                {calculateReputationPercentage(profile.reputation, 1000)}%
-              </li>
-            </ul>
-          </div>
+              <ul>
+                <li>
+                  Progress towards Endorsed: {calculateReputationPercentage(profile.reputation, 30)}
+                  %
+                </li>
+                <li>
+                  Progress towards Super Smarty Pants:{' '}
+                  {calculateReputationPercentage(profile.reputation, 100)}%
+                </li>
+                <li>
+                  Progress towards Mentor: {calculateReputationPercentage(profile.reputation, 500)}%
+                </li>
+                <li>
+                  Progress towards Grandmaster:{' '}
+                  {calculateReputationPercentage(profile.reputation, 1000)}%
+                </li>
+              </ul>
+            </div>
             <button onClick={toggleEditing}>Edit Profile</button>
           </div>
         )}
